60251fa1826ff364a167d83197c9c3c17852a53d
Matt Traudt <system33@mineoas.us>
2014-11-04 22:48:29 -0600

    add password policy class

---
+++ PasswordPolicy.cpp
@@ -0,0 +1,63 @@
+#include "PasswordPolicy.h"
+
+PasswordPolicy::PasswordPolicy()
+{
+    m_minimumLength = 0;
+    m_maximumLength = 0;
+    m_charClassMinimums = std::vector<int>(CHAR_CLASS_SIZE, -1);
+}
+
+void PasswordPolicy::enableClass(CharacterClass cc)
+{
+    if (cc >= CHAR_CLASS_SIZE)
+        throw IndexOutOfRangeException();
+    else if (m_charClassMinimums[cc] < 0)
+        m_charClassMinimums[cc] = 0;
+}
+
+void PasswordPolicy::setClassEnabled(CharacterClass cc, bool enabled)
+{
+    if (cc >= CHAR_CLASS_SIZE)
+        throw IndexOutOfRangeException();
+    else if (enabled && m_charClassMinimums[cc] < 0)
+        m_charClassMinimums[cc] = 0;
+    else if (!enabled)
+        m_charClassMinimums[cc] = -1;
+}
+
+bool PasswordPolicy::getClassEnabled(CharacterClass cc)
+{
+    if (cc >= CHAR_CLASS_SIZE)
+        throw IndexOutOfRangeException();
+    else
+        return (m_charClassMinimums[cc] >= 0);
+}
+
+void PasswordPolicy::setClassMinimum(CharacterClass cc, int min)
+{
+    if (cc >= CHAR_CLASS_SIZE)
+        throw IndexOutOfRangeException();
+    else
+        m_charClassMinimums[cc] = min;
+}
+
+bool PasswordPolicy::getClassMinimum(CharacterClass cc)
+{
+    if (cc >= CHAR_CLASS_SIZE)
+        throw IndexOutOfRangeException();
+    else
+        return m_charClassMinimums[cc];
+}
+
+void PasswordPolicy::setLength(int minLen, int maxLen)
+{
+    if (maxLen < minLen)
+        throw InvalidArgumentException();
+    else if (minLen < 0 || maxLen < 0)
+        throw InvalidArgumentException();
+    else
+    {
+        m_minimumLength = minLen;
+        m_maximumLength = maxLen;
+    }
+}
---
+++ PasswordPolicy.h
@@ -0,0 +1,39 @@
+#ifndef PASSWORDPOLICY_H
+#define PASSWORDPOLICY_H
+
+#include <vector>
+#include <string>
+#include "Exceptions.h"
+
+class PasswordPolicy
+{
+public:
+    enum CharacterClass
+    {
+        CHAR_CLASS_LOW_ALPHAS = 0,
+        CHAR_CLASS_HIGH_ALPHAS = 1,
+        CHAR_CLASS_DIGITS = 2,
+        CHAR_CLASS_SYMBOLS = 3,
+        CHAR_CLASS_SPACE = 4,
+        CHAR_CLASS_SIZE = 5
+    };
+    void enableClass(CharacterClass cc);
+    void setClassEnabled(CharacterClass cc, bool enabled);
+    void setClassDisabled(CharacterClass cc, bool disabled) {setClassEnabled(cc, !disabled);}
+    bool getClassEnabled(CharacterClass cc);
+    bool getClassDisabled(CharacterClass cc) {return !getClassEnabled(cc);}
+    void setClassMinimum(CharacterClass cc, int min);
+    bool getClassMinimum(CharacterClass cc);
+    void setLength(int length) {m_minimumLength = m_maximumLength = length;}
+    void setLength(int minLen, int maxLen);
+    unsigned int getMinLength() {return m_minimumLength;}
+    unsigned int getMaxLength() {return m_maximumLength;}
+    std::string generate() {return "";}
+    PasswordPolicy();
+private:
+    unsigned int m_minimumLength;
+    unsigned int m_maximumLength;
+    std::vector<int> m_charClassMinimums;
+};
+
+#endif // PASSWORDPOLICY_H
