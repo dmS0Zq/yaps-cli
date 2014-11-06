06edb9e62ce5306fb2cfe1dd3570cd48b438672b
Matt Traudt <system33@mineoas.us>
2014-11-06 07:48:06 -0600

    reorg generate func and finish it

--- PasswordPolicy.cpp
+++ PasswordPolicy.cpp
@@ -1,6 +1,5 @@
 #include "PasswordPolicy.h"
-
-#include <iostream>
+#include <algorithm>
 
 const std::vector<std::string> PasswordPolicy::CHARSETS = { "abcdefghijklmnopqrstuvwxyz",
                                                             "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
@@ -17,14 +16,6 @@ PasswordPolicy::PasswordPolicy()
     m_mode = NORMAL_MODE;
     m_specialCharset = "";
     m_charClassMinimums = std::vector<int>(CHAR_CLASS_SIZE, -1);
-}
-
-void PasswordPolicy::enableClass(CharacterClass cc)
-{
-    if (cc >= CHAR_CLASS_SIZE)
-        throw IndexOutOfRangeException();
-    else if (m_charClassMinimums[cc] < 0)
-        m_charClassMinimums[cc] = 0;
 }
 
 void PasswordPolicy::setClassEnabled(CharacterClass cc, bool enabled)
@@ -74,27 +65,99 @@ void PasswordPolicy::setLength(int minLen, int maxLen)
     }
 }
 
+void PasswordPolicy::enableAllClasses()
+{
+    for (unsigned int cc = 0; cc < CHAR_CLASS_SIZE; ++cc)
+        setClassEnabled(static_cast<CharacterClass>(cc));
+}
+
+void PasswordPolicy::disableAllClasses()
+{
+    for (unsigned int cc = 0; cc < CHAR_CLASS_SIZE; ++cc)
+        setClassDisabled(static_cast<CharacterClass>(cc));
+}
+
+void PasswordPolicy::requireAllClasses()
+{
+    for (unsigned int cc = 0; cc < CHAR_CLASS_SIZE; ++cc)
+        setClassRequired(static_cast<CharacterClass>(cc));
+}
+
 std::string PasswordPolicy::generate()
 {
-    std::vector<char> selectedChars;
-    std::uniform_int_distribution<unsigned int> distSelectedSize(m_minimumLength, m_maximumLength);
-    std::vector<std::uniform_int_distribution<unsigned int>> dists;
-    for (unsigned int cc = 0; cc < CHAR_CLASS_SIZE; ++cc)
-        dists.push_back(std::uniform_int_distribution<unsigned int>(0, CHARSETS[cc].length()-1));
-    unsigned int numChars = distSelectedSize(mtrand_64);
-    selectedChars.reserve(numChars);
-
-    for (unsigned int cc = 0; cc < CHAR_CLASS_SIZE; ++cc)
+    if (m_mode == NORMAL_MODE)
     {
-        for (int i = 0; i < getClassMinimum(static_cast<CharacterClass>(cc)); ++i)
+        unsigned int minCharsRequried = getSumOfClassMinimums();
+        if (minCharsRequried > m_maximumLength)
+            throw InvalidInternalStateException();
+        else if (!getAnyClassEnabled())
+            throw InvalidInternalStateException();
+
+        std::vector<char> selectedChars;
+        std::uniform_int_distribution<unsigned int> distSelectedSize(std::max(minCharsRequried, m_minimumLength), m_maximumLength);
+        unsigned int numChars = distSelectedSize(mtrand_64);
+        selectedChars.reserve(numChars);
+        // make distributions for randomly generating chars
+        std::vector<std::uniform_int_distribution<unsigned int>> dists;
+        for (unsigned int cc = 0; cc < CHAR_CLASS_SIZE; ++cc)
+            dists.push_back(std::uniform_int_distribution<unsigned int>(0, CHARSETS[cc].length()-1));
+        // add randomly selected chars to selected chars vector
+        for (unsigned int cc = 0; cc < CHAR_CLASS_SIZE; ++cc)
+            for (int i = 0; i < getClassMinimum(static_cast<CharacterClass>(cc)); ++i)
+                selectedChars.push_back(CHARSETS[cc].at(dists[cc](mtrand_64)));
+
+        if (selectedChars.size() < selectedChars.capacity())
         {
-            selectedChars.push_back(CHARSETS[cc].at(dists[cc](mtrand_64)));
+            std::string extraSet = "";
+            for (unsigned int cc = 0; cc < CHAR_CLASS_SIZE; ++cc)
+                if (getClassEnabled(static_cast<CharacterClass>(cc)))
+                    extraSet += CHARSETS[cc];
+            std::uniform_int_distribution<unsigned int> distExtraSet(0, extraSet.length()-1);
+            while (selectedChars.size() < selectedChars.capacity())
+            {
+                selectedChars.push_back(extraSet[distExtraSet(mtrand_64)]);
+            }
         }
+        std::shuffle(selectedChars.begin(), selectedChars.end(), mtrand_64);
+        std::string pw = "";
+        for (unsigned int i = 0; i < selectedChars.size(); i++)
+            pw += selectedChars[i];
+        return pw;
     }
+    else if (m_mode == SPECIAL_MODE)
+    {
+        if (m_specialCharset.length() == 0)
+            throw InvalidInternalStateException();
 
-    std::string pw = "";
+        std::vector<char> selectedChars;
+        std::uniform_int_distribution<unsigned int> distSize(m_minimumLength, m_maximumLength);
+        unsigned int numChars = distSize(mtrand_64);
+        std::uniform_int_distribution<unsigned int> distSpecialCharset(0, m_specialCharset.length()-1);
+        for (unsigned int i = 0; i < numChars; i++)
+            selectedChars.push_back(m_specialCharset.at(distSpecialCharset(mtrand_64)));
+        std::shuffle(selectedChars.begin(), selectedChars.end(), mtrand_64);
+        std::string pw = "";
+        for (unsigned int i = 0; i < selectedChars.size(); i++)
+            pw += selectedChars[i];
+        return pw;
+    }
+    else
+        throw InvalidInternalStateException();
+}
 
-    for (unsigned int i = 0; i < selectedChars.size(); i++)
-        pw += selectedChars[i];
-    return pw;
+unsigned int PasswordPolicy::getSumOfClassMinimums()
+{
+    unsigned int val = 0;
+    for (unsigned int cc = 0; cc < CHAR_CLASS_SIZE; ++cc)
+        if (getClassRequired(static_cast<CharacterClass>(cc)))
+            val += getClassMinimum(static_cast<CharacterClass>(cc));
+    return val;
+}
+
+bool PasswordPolicy::getAnyClassEnabled()
+{
+    for (unsigned int cc = 0; cc < CHAR_CLASS_SIZE; ++cc)
+        if (getClassEnabled(static_cast<CharacterClass>(cc)))
+            return true;
+    return false;
 }
--- PasswordPolicy.h
+++ PasswordPolicy.h
@@ -24,18 +24,27 @@ public:
         NORMAL_MODE     = 0,
         SPECIAL_MODE    = 1
     };
-    void enableClass(CharacterClass cc);
-    void setClassEnabled(CharacterClass cc, bool enabled);
-    void setClassDisabled(CharacterClass cc, bool disabled) {setClassEnabled(cc, !disabled);}
-    bool getClassEnabled(CharacterClass cc);
-    bool getClassDisabled(CharacterClass cc) {return !getClassEnabled(cc);}
-    void setClassMinimum(CharacterClass cc, int min);
-    int getClassMinimum(CharacterClass cc);
-    void setLength(int length) {m_minimumLength = m_maximumLength = length;}
-    void setLength(int minLen, int maxLen);
-    unsigned int getMinLength() {return m_minimumLength;}
-    unsigned int getMaxLength() {return m_maximumLength;}
-    std::string generate();
+    void         setClassEnabled  (CharacterClass cc, bool enabled = true);
+    bool         getClassEnabled  (CharacterClass cc);
+    void         setClassDisabled (CharacterClass cc, bool disabled = true) {setClassEnabled(cc, !disabled);}
+    bool         getClassDisabled (CharacterClass cc)                       {return !getClassEnabled(cc);}
+    void         setClassMinimum  (CharacterClass cc, int min);
+    int          getClassMinimum  (CharacterClass cc);
+    void         setClassRequired (CharacterClass cc)                       {setClassMinimum(cc, 1);}
+    bool         getClassRequired (CharacterClass cc)                       {return getClassMinimum(cc) >= 1;}
+    void         setLength        (int length)                              {m_minimumLength = m_maximumLength = length;}
+    void         setLength        (int minLen, int maxLen);
+    unsigned int getMinLength     ()                                        {return m_minimumLength;}
+    unsigned int getMaxLength     ()                                        {return m_maximumLength;}
+    void         setMode          (GenerateMode gm)                         {m_mode = gm;}
+    GenerateMode getMode          ()                                        {return m_mode;}
+    void         setSpecialCharset(std::string charset)                     {m_specialCharset = charset;}
+    std::string  getSpecialCharset()                                        {return m_specialCharset;}
+    void         enableAllClasses ();
+    void         disableAllClasses();
+    void         requireAllClasses();
+    unsigned int numberOfClasses  ()                                        {return CHAR_CLASS_SIZE;}
+    std::string  generate         ();
     PasswordPolicy();
 private:
     GenerateMode m_mode;
@@ -46,6 +55,8 @@ private:
     static const std::vector<std::string> CHARSETS;
     static std::random_device rd;
     static std::mt19937_64 mtrand_64;
+    unsigned int getSumOfClassMinimums();
+    bool getAnyClassEnabled();
 };
 
 #endif // PASSWORDPOLICY_H
